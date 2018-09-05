d8a6f1d6569dae1779ef9a3e91228b47abe2983e
jewels98 <mrwright0825@gmail.com>
2018-09-05 15:05:56 -0400

    Audio complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,45 +1,89 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
 
-
-
 class Album extends Component {
   constructor(props) {
     super(props);
 
-      const album = albumData.find(album => {
-        return album.slug === this.props.match.params.slug
-      });
+    const album = albumData.find( album => {
+      return album.slug === this.props.match.params.slug
+    });
 
-      this.state = {
-        album: album
-      };
+    this.state = {
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false,
+      currentlyHovered: false
+    };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
   }
 
-   render() {
-     return (
-       <section className="album">
-          <section id="album-info">
-           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-           <div className="album-details">
-             <h1 id="album-title">{this.state.album.title}</h1>
-             <h2 className="artist">{this.state.album.artist}</h2>
-             <div id="release-info">{this.state.album.releaseInfo}</div>
-           </div>
-         </section>
-         <table id="song-list">
-           <colgroup>
-             <col id="song-number-column" />
-             <col id="song-title-column" />
-             <col id="song-duration-column" />
-           </colgroup>  
-           <tbody>
-           </tbody>
-         </table>
-       </section>
-     );
-   }
- }
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
 
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
+  }
+
+  render() {
+    return (
+      <section className="album">
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody>
+          {this.state.album.songs.map( (song, index) =>
+            <tr className="song" key={index} onClick={() => this.handleSongClick(song)}
+            onMouseEnter={() => this.setState({currentlyHovered: index+1})}
+            onMouseLeave={() => this.setState({currentlyHovered: false})} >
+              <td>
+                {(this.state.currentSong.title === song.title) ?
+                  <span className={this.state.isPlaying ? "ion-pause" : "ion-play"}></span> :
+                  (this.state.currentlyHovered === index+1) ?
+                  <span className="ion-play"></span> :
+                  <span className="song-number">{index+1}</span>
+                }
+               </td>
+              <td>{song.title}</td>
+              <td>{song.duration}</td>
+            </tr>
+          )}
+          </tbody>
+        </table>
+      </section>
+    );
+  }
+}
 
 export default Album;
