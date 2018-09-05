4091cb7397c48a37abda5b008c4927ea6504ff47
jewels98 <mrwright0825@gmail.com>
2018-09-05 13:24:40 -0400

    Album complete

---
+++ src/components/Album.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import albumData from './../data/albums';
+
+
+const Album = () => (
+  <section className="album">
+    Album page will go here
+  </section>
+);
+
+export default Album;
